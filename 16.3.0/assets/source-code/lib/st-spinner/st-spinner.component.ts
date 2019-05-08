c76f9a6d312f247464ce18f60a369dfcbb8a185d
jdiaz-stratio <jdiaz@stratio.com>
2019-05-08 11:19:40 +0200

    docs(release 16.3.0): Add documentation

---
+++ 16.3.0/assets/source-code/lib/st-spinner/st-spinner.component.ts
@@ -0,0 +1,46 @@
+/*
+ * © 2017 Stratio Big Data Inc., Sucursal en España.
+ *
+ * This software is licensed under the Apache License, Version 2.0.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the terms of the License for more details.
+ *
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+/**
+ * @description {Component} Spinner
+ *
+ * This component shows a spinner when something is being loaded
+ *
+ * @example
+ *
+ * {html}
+ *
+ * ```
+ *    <st-spinner theme="secondary">
+ *    </st-spinner>
+ * ```
+ */
+@Component({
+   selector: 'st-spinner',
+   templateUrl: './st-spinner.component.html',
+   styleUrls: ['./st-spinner.component.scss'],
+   changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class StSpinnerComponent {
+   /** @input {string} [theme='primary'] Name of the spinner theme. By default it is 'primary' */
+   @Input() theme: string = 'primary';
+
+   getClasses(): any {
+      const result: any = {path: true};
+      if (this.theme) {
+         const themeClass = 'path--' + this.theme;
+         result[themeClass] = true;
+      }
+      return result;
+   }
+}
